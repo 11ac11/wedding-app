7111bc70f117471404f890396717be0eab95dd75
11ac11 <alexander.crump11@gmail.com>
2024-01-22 09:51:44 +0100

    commented for now

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,73 +1,73 @@
-'use client'
+// 'use client'
 
-import { useEffect, useState } from 'react'
-import { toast } from 'react-hot-toast'
-import { useDebounce } from 'use-debounce'
+// import { useEffect, useState } from 'react'
+// import { toast } from 'react-hot-toast'
+// import { useDebounce } from 'use-debounce'
 
 
-export function Search({ searchGuests }) {
-  const [query, setQuery] = useState('')
-  const [searchResults, setSearchResults] = useState([])
-  const [debouncedQuery] = useDebounce(query, 150)
+// export function Search({ searchGuests }) {
+//   const [query, setQuery] = useState('')
+//   const [searchResults, setSearchResults] = useState([])
+//   const [debouncedQuery] = useDebounce(query, 150)
 
-  useEffect(() => {
-    let current = true
-    if (debouncedQuery.trim().length > 0) {
-      searchGuests(debouncedQuery).then((results) => {
-        if (current) {
-          setSearchResults(results)
-        }
-      })
-    }
-    return () => {
-      current = false
-    }
-  }, [debouncedQuery, searchGuests])
+//   useEffect(() => {
+//     let current = true
+//     if (debouncedQuery.trim().length > 0) {
+//       searchGuests(debouncedQuery).then((results) => {
+//         if (current) {
+//           setSearchResults(results)
+//         }
+//       })
+//     }
+//     return () => {
+//       current = false
+//     }
+//   }, [debouncedQuery, searchGuests])
 
-  return (
-    <div className="w-full">
-      <Command label="Command Menu" shouldFilter={false} className="h-[200px]">
-        <CommandInput
-          id="search"
-          placeholder="Search for Pokémon"
-          className="focus:ring-0 sm:text-sm text-base focus:border-0 border-0 active:ring-0 active:border-0 ring-0 outline-0"
-          value={query}
-          onValueChange={(q) => setQuery(q)}
-        />
-        <CommandList>
-          <CommandEmpty>No results found.</CommandEmpty>
-          {searchResults.map((pokemon) => (
-            <CommandItem
-              key={pokemon.id}
-              value={pokemon.name}
-              className="data-[selected='true']:bg-zinc-50  flex items-center justify-between py-3"
-              onSelect={(p) => {
-                console.log(p)
-                toast.success(`You selected ${p}!`)
-              }}
-            >
-              <div className="flex items-center space-x-4">
-                <div className="space-y-1">
-                  <p className="text-sm text-gray-500">
-                    {pokemon.name.substring(0, 90)}
-                  </p>
-                </div>
-              </div>
-              <div className="text-sm text-gray-500">
-                {pokemon.similarity ? (
-                  <div className="text-xs font-mono p-0.5 rounded bg-zinc-100">
-                    {pokemon.similarity.toFixed(3)}
-                  </div>
-                ) : (
-                  <div />
-                )}
-              </div>
-            </CommandItem>
-          ))}
-        </CommandList>
-      </Command>
-    </div>
-  )
-}
+//   return (
+//     <div className="w-full">
+//       <Command label="Command Menu" shouldFilter={false} className="h-[200px]">
+//         <nput
+//           id="search"
+//           placeholder="Search for Pokémon"
+//           className="focus:ring-0 sm:text-sm text-base focus:border-0 border-0 active:ring-0 active:border-0 ring-0 outline-0"
+//           value={query}
+//           onValueChange={(q) => setQuery(q)}
+//         />
+//         <CommandList>
+//           <CommandEmpty>No results found.</CommandEmpty>
+//           {searchResults.map((pokemon) => (
+//             <CommandItem
+//               key={pokemon.id}
+//               value={pokemon.name}
+//               className="data-[selected='true']:bg-zinc-50  flex items-center justify-between py-3"
+//               onSelect={(p) => {
+//                 console.log(p)
+//                 toast.success(`You selected ${p}!`)
+//               }}
+//             >
+//               <div className="flex items-center space-x-4">
+//                 <div className="space-y-1">
+//                   <p className="text-sm text-gray-500">
+//                     {pokemon.name.substring(0, 90)}
+//                   </p>
+//                 </div>
+//               </div>
+//               <div className="text-sm text-gray-500">
+//                 {pokemon.similarity ? (
+//                   <div className="text-xs font-mono p-0.5 rounded bg-zinc-100">
+//                     {pokemon.similarity.toFixed(3)}
+//                   </div>
+//                 ) : (
+//                   <div />
+//                 )}
+//               </div>
+//             </CommandItem>
+//           ))}
+//         </CommandList>
+//       </Command>
+//     </div>
+//   )
+// }
 
-Search.displayName = 'Search'
+// Search.displayName = 'Search'

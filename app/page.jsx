c1940673d6bfe186903ec9280ba60c4df3ce6896
11ac11 <alexander.crump11@gmail.com>
2024-01-25 16:07:56 +0100

    added image to homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,6 @@
+import Image from "next/image"
+import hotelTurret from "../public/images/hotel-turret.png"
+
 export const runtime = 'edge'
 export const preferredRegion = 'home'
 export const dynamic = 'force-dynamic'
@@ -6,7 +9,8 @@ export default async function App() {
 
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center">
-      <h1>This is App</h1>
+      <h1>We're getting married!</h1>
+      <Image src={hotelTurret} quality={100} style={{ borderRadius: '1rem' }}/>
     </main>
   )
 }

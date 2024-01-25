3137c7da6f31d4b40daf105cb949681c8191044d
11ac11 <alexander.crump11@gmail.com>
2024-01-25 16:11:56 +0100

    changed to template lit

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,7 @@ export default async function App() {
 
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center">
-      <h1>We're getting married!</h1>
+      <h1>{ `We're getting married!`}</h1>
       <Image src={hotelTurret} quality={100} style={{ borderRadius: '1rem' }}/>
     </main>
   )

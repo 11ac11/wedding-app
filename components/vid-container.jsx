27da489573ca2e7d107868cc9da34486c5e58ffd
11ac11 <alexander.crump11@gmail.com>
2024-02-17 15:38:16 +0100

    fixed video for mobile

--- components/vid-container.jsx
+++ components/vid-container.jsx
@@ -12,12 +12,24 @@ const VidContainer = styled.div`
     z-index: 0;
     object-fit: cover;
   }
+
+  @media (max-width: 600px) {
+    /* Adjust styles for screens with a width of 767 pixels or less (typical mobile screens) */
+    width: 300px;
+    height: 300px;
+  }
+
+  @media (max-width: 350px) {
+    /* Adjust styles for screens with a width of 767 pixels or less (typical mobile screens) */
+    width: 250px;
+    height: 250px;
+  }
 `
 
 export default function VidCont({ src, fill }) {
   return (
     <VidContainer>
-      <video width="100%" height="100%" autoPlay loop muted preload="auto">
+      <video width="100%" height="100%" autoPlay loop muted preload="auto" playsInline>
         <source src="./images/IMG_8025.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>

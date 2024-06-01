b253b283ecb4ab34c531a3b8e87f04ae4a3a68f5
11ac11 <alexander.crump11@gmail.com>
2024-06-01 11:18:11 +0200

    added missing useState, useEffect imports

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState, useEffect } from 'react';
 // import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';

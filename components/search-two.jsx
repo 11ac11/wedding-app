c582ec0dee7f12ee0937cbaa4cd5f0f8ccec1fd0
11ac11 <alexander.crump11@gmail.com>
2024-06-01 17:59:59 +0200

    changed search container animation to styled components

--- components/search-two.jsx
+++ components/search-two.jsx
@@ -1,6 +1,40 @@
 'use client';
 
 import { useDebouncedCallback } from 'use-debounce';
+import styled, { css } from 'styled-components';
+
+const SearchContainer = styled.div`
+  height: ${({ $editingGuestId }) => $editingGuestId ? '0' : '1rem'};
+  height: ${({ $editingGuestId }) => $editingGuestId ? '0' : '110px'};
+  opacity: ${({ $editingGuestId }) => $editingGuestId ? 0 : 1};
+  width: 300px;
+  overflow: hidden;
+  transition: height 0.2s ease-in, opacity 0.2s ease-in, margin 0.2s ease-in;
+
+  & label {
+    display: block;
+    width: 100%;
+    text-align: center;
+  }
+`
+
+const SearchInput = styled.input`
+  padding: 0.1rem 1rem;
+  border-radius: 6px;
+  line-height: 1.4;
+  font-size: 1rem;
+  text-transform: uppercase;
+  letter-spacing: 3px;
+  margin: 1rem 0;
+  caret-color: var(--slategrey);
+
+  &:focus-visible,
+  &:hover {
+    outline: 2px solid var(--gold);
+    outline-offset: -2px;
+    transition: outline 0.3s ease;
+  }
+`
 
 export default function Search({ searchTerm, setSearchTerm, editingGuestId }) {
   const handleSearch = useDebouncedCallback((term) => {
@@ -13,18 +47,18 @@ export default function Search({ searchTerm, setSearchTerm, editingGuestId }) {
 
   return (
     <>
-      <div className={`search-container ${!!editingGuestId ? 'fade-out-shrink' : null}`}>
-        <label htmlFor="search" className="sr-only">
+      <SearchContainer $editingGuestId={!!editingGuestId}>
+        <label htmlFor="search">
           Search for the full names of your party:
         </label>
-        <input
+        <SearchInput
           className="search"
           placeholder={''}
           onChange={(e) => handleSearch(e.target.value)}
           defaultValue={searchTerm}
         />
         {/* <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" /> */}
-      </div>
+      </SearchContainer>
     </>
   );
 }

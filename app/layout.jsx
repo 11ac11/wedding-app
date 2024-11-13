02f2164aef85b5c34b5e8af271a60da3f5564ce9
11ac11 <alexander.crump11@gmail.com>
2024-11-13 11:19:29 +0100

    added appleWebApp to metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,8 +4,18 @@ import StyledJsxRegistry from '../lib/registry'
 export const metadata = {
   metadataBase: new URL('https://postgres-starter.vercel.app'),
   title: `Robyn + Alex's Wedding`,
-  description:
-    'September 2025',
+  description: '5 September 2025',
+  appleWebApp: {
+    title: `Robyn + Alex's Wedding`,
+    statusBarStyle: 'black-translucent',
+    startupImage: [
+      '/public/images/granada-circle.png',
+      {
+        url: '/public/images/granada-circle.png',
+        media: '(device-width: 768px) and (device-height: 1024px)',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({ children }) {

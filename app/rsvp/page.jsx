d8c132d2e60030c0bd67eddf9d86f751ce29694d
11ac11 <alexander.crump11@gmail.com>
2024-11-13 19:25:22 +0100

    standalone apple PWA working on all pages

--- app/rsvp/page.jsx
+++ app/rsvp/page.jsx
@@ -3,7 +3,6 @@ import RsvpClient from './rsvp-client.jsx'
 export const metadata = {
   metadataBase: new URL('https://postgres-starter.vercel.app'),
   title: `RSVP`,
-  description: '5 September 2025',
   appleWebApp: {
     title: `Robyn + Alex's Wedding`,
     statusBarStyle: 'black-translucent',

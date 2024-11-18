219f82648b946e07e00760d1e52f4e7ace63cfaa
11ac11 <alexander.crump11@gmail.com>
2024-11-18 15:19:53 +0100

    removed width from countdown

--- components/Countdown.jsx
+++ components/Countdown.jsx
@@ -4,7 +4,6 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  width: 400px;
   margin: auto;
   display: flex;
   flex-direction: column;

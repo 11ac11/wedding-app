15395542dadfa512b73165bc15cb5c10b05cc969
11ac11 <alexander.crump11@gmail.com>
2025-08-29 11:41:34 +0200

    added more faqs

--- app/faqs/page.jsx
+++ app/faqs/page.jsx
@@ -20,6 +20,11 @@ export default async function FAQs() {
       <Image src={Iceland} alt="Iceland" fill={true} />
       <h1>FAQs</h1>
       <div className="info-section">
+        <h2 className="uppercase">Hotel Breakfast</h2>
+        <p>{`Breakfast at the hotel is 17.50€ per person, and is from 07:00am-10.30am every day.`}</p>
+        <p>{`As the hotel is on a hill, not near any cafes and the town is 15 minute walk away, we would recommend getting breakfast at the venue. That is, unless you don't mind getting a taxi or the challenging walk back!`}</p>
+      </div>
+      <div className="info-section">
         <h2 className="uppercase">RSVP deadline</h2>
         <p>{`Please RSVP by 1st June 2025.`}</p>
       </div>
@@ -27,12 +32,12 @@ export default async function FAQs() {
         <h2 className="uppercase">Dresscode</h2>
         <p>{`Smart-casual, as we expect it to be warm in early September. Ties and blazers optional.`}</p>
         <p className="bold">
-          {`Ladies: please avoid wearing white or burgundy so that we don't confuse you with the bridal party!`}
+          {`Ladies: please avoid wearing white or burgundy/copper so that we don't confuse you with the bridal party!`}
         </p>
       </div>
       <div className="info-section">
         <h2 className="uppercase">Local Taxis</h2>
-        <p>{`You can use the taxi applications Uber, Cabify or Freenow.`}</p>
+        <p>{`You can use the taxi applications Uber, Cabify or Freenow, although they are not as available as they are in Barcelona city centre. Alternatively, you can ask the hotel to book one for you or call yourself.`}</p>
         <p>
           {`The local Castelldefels taxi number is: `}
           <a href="tel:+34956652222" className="inline-link">
@@ -45,6 +50,10 @@ export default async function FAQs() {
             taxifels.es/en/
           </a>
         </p>
+      </div>
+      <div className="info-section">
+        <h2 className="uppercase">Phones during ceremony</h2>
+        <p>{`We welcome people using phones for photos and videos at the ceremony, but please ensure all phones are on silent.`}</p>
       </div>
       <div className="info-section">
         <h2 className="uppercase">Social Media</h2>
